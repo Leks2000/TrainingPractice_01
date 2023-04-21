27e7c3e78506cf7093350684a299bc75e3502c9d
Leks2000 <107043945+Leks2000@users.noreply.github.com>
2023-04-22 01:21:37 +0300

    Add files via upload

---
+++ HA_Task_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HA_Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resultGold;
+            int rate = 3;
+            Console.WriteLine("Введите количество вашего золота ");
+            int inventory = int.Parse(Console.ReadLine());
+            Console.WriteLine("Курс валюты: 3 золота за 1 кристал\n\n" + "Сколько вы хотите купить кристаллов? ");
+            int buyCount = int.Parse(Console.ReadLine());
+            resultGold = inventory >= buyCount * rate;
+            buyCount *= Convert.ToInt32(resultGold);
+            inventory -= buyCount * rate;
+            Console.WriteLine($"Количество кристалов: {buyCount} остаток золота: {inventory}");
+            Console.ReadKey();
+        }
+    }
+}
