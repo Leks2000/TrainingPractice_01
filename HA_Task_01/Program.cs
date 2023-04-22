cca0dd65f006961c8e46abac51f296bbbf914ad7
Leks2000 <107043945+Leks2000@users.noreply.github.com>
2023-04-22 11:34:25 +0300

    Add files via upload

--- HA_Task_01/Program.cs
+++ HA_Task_01/Program.cs
@@ -10,17 +10,41 @@ namespace HA_Task_01
     {
         static void Main(string[] args)
         {
-            bool resultGold;
+            bool resultGold = false;
             int rate = 3;
+            while (resultGold == false)
+            {
+            Console.Clear();
+            gold:
             Console.WriteLine("Введите количество вашего золота ");
             int inventory = int.Parse(Console.ReadLine());
+            if (inventory < 0)
+            {
+                Console.WriteLine("Количество кристаллов не может быть равно 0, введите число снова");
+                Console.ReadLine();
+                Console.Clear();
+                goto gold;
+            }
+            cristalls:
             Console.WriteLine("Курс валюты: 3 золота за 1 кристал\n\n" + "Сколько вы хотите купить кристаллов? ");
             int buyCount = int.Parse(Console.ReadLine());
+            if(buyCount < 0)
+            {
+                Console.WriteLine("Количество кристаллов не может быть равно 0, введите число снова");
+                Console.ReadLine();
+                Console.Clear();
+                goto cristalls;
+            }
             resultGold = inventory >= buyCount * rate;
             buyCount *= Convert.ToInt32(resultGold);
             inventory -= buyCount * rate;
             Console.WriteLine($"Количество кристалов: {buyCount} остаток золота: {inventory}");
+                if (resultGold == false)
+                {
+                    Console.WriteLine("Недостаточно средств для конвертации валюты");
+                }
             Console.ReadKey();
+            }
         }
     }
 }
